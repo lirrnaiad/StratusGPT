a3d87f28c791aa63023de386643d6eb55ad8ee37
seanivanfabia@gmail.com <99702949+lirrnaiad@users.noreply.github.com>
2025-02-20 18:50:00 +0800

    Add main method

--- bot.py
+++ bot.py
@@ -3,28 +3,32 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
-
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-intents = discord.Intents.default()
-intents.message_content = True
+def main():
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+    bot = commands.Bot(command_prefix="!", intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
-    print('------')
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user}')
+        print('------')
 
-    await bot.load_extension("cogs.stratusgpt")
-    await bot.load_extension("cogs.weather")
-    await bot.load_extension("cogs.prompt")
-    await bot.load_extension("cogs.wprompt")
+        await bot.load_extension("cogs.stratusgpt")
+        await bot.load_extension("cogs.weather")
+        await bot.load_extension("cogs.prompt")
+        await bot.load_extension("cogs.wprompt")
 
 
-@bot.event
-async def on_message(message):
-    await bot.process_commands(message)
+    @bot.event
+    async def on_message(message):
+        await bot.process_commands(message)
 
-bot.run(TOKEN)
+    bot.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
